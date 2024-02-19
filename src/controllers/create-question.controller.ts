71fa98dd1441bacc261523d4f6488a8a58a030a7
Arthur Kellermann <arthur.kellermann956@gmail.com>
2024-02-19 12:36:14 -0300

    feat: add current user decorator

--- src/controllers/create-question.controller.ts
+++ src/controllers/create-question.controller.ts
@@ -1,17 +1,15 @@
 import { Controller, Post, UseGuards } from '@nestjs/common';
-import { JwtService } from '@nestjs/jwt';
-import { PrismaService } from 'src/prisma/prisma.service';
+import { CurrentUser } from 'src/auth/current-user-decorator';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { UserPayload } from 'src/auth/jwt.strategy';
 
 @Controller('/questions')
 @UseGuards(JwtAuthGuard)
 export class CreateQuestionController {
-  constructor(
-    private prisma: PrismaService,
-    private jwt: JwtService,
-  ) {}
+  constructor() {}
   @Post()
-  async handle() {
+  async handle(@CurrentUser() user: UserPayload) {
+    console.log(user);
     return 'ok';
   }
 }
